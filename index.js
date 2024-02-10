b31633aa09a2c845c3c5e7ec4a151eb7b6d4fbaa
amart302 <154788842+amart302@users.noreply.github.com>
2024-02-10 19:03:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+function delet(){
+    let a = document.getElementById('inpCal');
+    let b = document.getElementById('console');
+    a.value = ''
+    b.textContent = ''
+}
+function prosent(){
+    let a = document.getElementById('inpCal');
+    a.value += '%'
+}
+function scb1(){
+    let a = document.getElementById('inpCal');
+    a.value += '('
+}
+function scb2(){
+    let a = document.getElementById('inpCal');
+    a.value += ')'
+}
+function star(){
+    let a = document.getElementById('inpCal');
+    a.value += '*'
+}
+function minus(){
+    let a = document.getElementById('inpCal');
+    a.value += '-'
+}
+function plus(){
+    let a = document.getElementById('inpCal');
+    a.value += '+'
+}
+function slech(){
+    let a = document.getElementById('inpCal');
+    a.value += '/'
+}
+function tochka(){
+    let a = document.getElementById('inpCal');
+    a.value += '.'
+}
+
+function seven(){
+    let a = document.getElementById('inpCal');
+    a.value += 7;
+}
+function eight(){
+    let a = document.getElementById('inpCal');
+    a.value += 8;
+}
+function nine(){
+    let a = document.getElementById('inpCal');
+    a.value += 9;
+}
+function six(){
+    let a = document.getElementById('inpCal');
+    a.value += 6;
+}
+function five(){
+    let a = document.getElementById('inpCal');
+    a.value += 5;
+}
+function four(){
+    let a = document.getElementById('inpCal');
+    a.value += 4;
+}
+function three(){
+    let a = document.getElementById('inpCal');
+    a.value += 3;
+}
+function two(){
+    let a = document.getElementById('inpCal');
+    a.value += 2;
+}
+function one(){
+    let a = document.getElementById('inpCal');
+    a.value += 1;
+}
+function zero(){
+    let a = document.getElementById('inpCal');
+    a.value += 0;
+}
+function twoZero(){
+    let a = document.getElementById('inpCal');
+    a.value += '00';
+}
+
+
+function result(){
+    let a = document.getElementById('inpCal').value;
+    let b = document.getElementById('console');
+    let ch = /\d/;
+    if(a.match(ch) == null){
+        alert('Введите пожалуйста число');
+    }else{
+        b.textContent = eval(a).toFixed(1);
+    }   
+}
+
+
+
+
+
+// function gg(){
+//     let a = document.getElementById('inp-cal').value;
+//     console.log(eval(a))
+// }
